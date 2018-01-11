7fdd6866bc4a09435176cb5447adf0fa8288eb34
BrandonWortmann <31518730+BrandonWortmann@users.noreply.github.com>
2018-01-11 13:52:27 -0700

    I started working on the number panel

--- src/calculator/view/NumberPanel.java
+++ src/calculator/view/NumberPanel.java
@@ -1,10 +1,15 @@
 package calculator.view;
 
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import java.awt.Color;
+import calculator.controller.CalculatorController;
 
 
-public class NumberPanel
+
+public class NumberPanel extends JPanel
 {
+	private JTextArea numberArea;
+	private CalculatorController appController;
 
 }
